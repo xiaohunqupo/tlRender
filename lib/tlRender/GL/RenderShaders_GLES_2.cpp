a6d463d71ba3de44e053fc10de5fe179fb90da4d
Darby Johnston <darbyjohnston@yahoo.com>
2025-11-22 16:41:51 -0800

    Build fix

--- lib/tlRender/GL/RenderShaders_GLES_2.cpp
+++ lib/tlRender/GL/RenderShaders_GLES_2.cpp
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright Contributors to the tlRender project.
 
-#include <tlTimelineGL/RenderPrivate.h>
+#include <tlGL/RenderPrivate.h>
 
 #include <ftk/Core/Format.h>
 
