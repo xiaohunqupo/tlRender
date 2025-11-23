38f7c2e09703d3ee41879d5ee23f592b117b266b
Darby Johnston <darbyjohnston@yahoo.com>
2025-11-23 11:46:47 -0800

    Add Python bindings

--- python/lib/TimelinePy/Bindings.cpp
+++ python/lib/TimelinePy/Bindings.cpp
@@ -3,7 +3,12 @@
 
 #include <TimelinePy/Bindings.h>
 
+#include <tlRender/Timeline/Timeline.h>
+
+#include <ftk/Core/Context.h>
+
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 namespace py = pybind11;
 
@@ -11,5 +16,14 @@ namespace tl
 {
     void timelineBind(py::module_& m)
     {
+        py::class_<timeline::Timeline, std::shared_ptr<timeline::Timeline> >(m, "Timeline")
+            .def(
+                py::init(py::overload_cast<
+                    const std::shared_ptr<ftk::Context>&,
+                    const ftk::Path&,
+                    const timeline::Options&>(&timeline::Timeline::create)),
+                py::arg("context"),
+                py::arg("path"),
+                py::arg("options") = timeline::Options());
     }
 }
