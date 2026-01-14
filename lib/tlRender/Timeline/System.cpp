249ebc9a34d5b18c956fddc0e092c70109f3e85c
Darby Johnston <darbyjohnston@yahoo.com>
2026-01-14 15:15:39 -0800

    Add logging

--- lib/tlRender/Timeline/System.cpp
+++ lib/tlRender/Timeline/System.cpp
@@ -3,9 +3,12 @@
 
 #include <tlRender/Timeline/System.h>
 
+#include <tlRender/Timeline/ColorOptions.h>
 #include <tlRender/Timeline/Player.h>
 
 #include <ftk/Core/Context.h>
+#include <ftk/Core/Format.h>
+#include <ftk/Core/String.h>
 
 namespace tl
 {
@@ -19,6 +22,14 @@ namespace tl
         _p(new Private)
     {
         FTK_P();
+        const auto lutNames = getLUTFormatNames();
+        const auto lutExts = getLUTFormatExts();
+        std::vector<std::string> s;
+        for (size_t i = 0; i < lutNames.size() && i < lutExts.size(); ++i)
+        {
+            s.push_back(ftk::Format("    * {0}: {1}").arg(lutNames[i]).arg(lutExts[i]));
+        }
+        _log(ftk::Format("Supported LUT formats:\n{0}").arg(ftk::join(s, '\n')));
     }
 
     System::~System()
