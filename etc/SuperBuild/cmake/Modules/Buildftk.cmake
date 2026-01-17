94d9d7bd504ac9bd3ba09bdfa999ffd917d62dda
Darby Johnston <darbyjohnston@yahoo.com>
2026-01-16 16:05:54 -0800

    Update feather-tk version

--- etc/SuperBuild/cmake/Modules/Buildftk.cmake
+++ etc/SuperBuild/cmake/Modules/Buildftk.cmake
@@ -1,7 +1,7 @@
 include(ExternalProject)
 
 set(ftk_GIT_REPOSITORY "https://github.com/grizzlypeak3d/feather-tk.git")
-set(ftk_GIT_TAG "de71bf252819fce67e55707acf3385759a6f15e7")
+set(ftk_GIT_TAG "67bff901cb11dcf4a8ace24bd5f3b7d92bf682c5")
 
 set(ftk_DEPS ZLIB PNG)
 set(ftk_ARGS
