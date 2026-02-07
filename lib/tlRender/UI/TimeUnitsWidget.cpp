270d495022b8c5352ad4509e7ba660816464d1bb
Darby Johnston <darbyjohnston@yahoo.com>
2026-02-07 13:44:56 -0800

    Update icons

--- lib/tlRender/UI/TimeUnitsWidget.cpp
+++ lib/tlRender/UI/TimeUnitsWidget.cpp
@@ -106,7 +106,7 @@ namespace tl
             p.model = model;
 
             p.button = ftk::ToolButton::create(context, shared_from_this());
-            p.button->setText("TU");
+            p.button->setIcon("Time");
             p.button->setPopupIcon(true);
 
             p.button->setPressedCallback(
